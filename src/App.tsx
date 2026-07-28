384199295665c895948d7f3da3b33aa41376d5d2
sebastianpenning <whimizera@mailbox.org>
2026-07-28 12:35:21 +0200

    css full screen setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 function App() {
 
     return (
-        <div>
-            <h1>test</h1>
+        <div id="main-container" className="min-w-full min-h-full">
+            <div className="flex">
+                <div></div>
+                <div>
+                    <h1>test</h1>
+                </div>
+            </div>
         </div>
     )
 }
