325a865407c8eb2f067310e98356eea396493026
sebastianpenning <whimizera@mailbox.org>
2026-07-28 13:13:38 +0200

    added font

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,14 @@ function App() {
             <div id='center-container' className="bg-sky-400 w-1/2">
                 <div id="center-text-container" className="flex justify-center min-h-full">
                     <div id='vertical-container' className="flex flex-col text-center justify-evenly min-h-full">
-                        <h1>portfolio</h1>
-                        <h1>project-1</h1>
-                        <h1>project-2</h1>
-                        <h1>project-3</h1>
+                        <div id="portfolio-title-container">
+                            <h1 className="text-8xl text-violet-200 font-micro">PORTFOLIO</h1>
+                            <h2>Sebastian Penning</h2>
+                        </div>
+                        <h1>Great Northern Peaks</h1>
+                        <h1>Transcendence</h1>
+                        <h1>miniRT</h1>
+                        <h1>FDF</h1>
                     </div>
                 </div>
             </div>
