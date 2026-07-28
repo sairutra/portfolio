7eb4b4b05bf8d68e6b355beb19121d697ede3fd8
sebastianpenning <whimizera@mailbox.org>
2026-07-28 12:48:28 +0200

    scaffolding of main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 function App() {
 
     return (
-        <div id="main-container" className="min-w-full min-h-full">
-            <div className="flex">
-                <div></div>
-                <div>
-                    <h1>test</h1>
+        <div id="main-container" className="min-w-full min-h-full flex justify-center bg-emerald-300">
+            <div id='center-container' className="bg-sky-400 w-1/2">
+                <div id="center-text-container" className="flex justify-center min-h-full">
+                    <div id='vertical-container' className="flex flex-col text-center justify-evenly min-h-full">
+                        <h1>portfolio</h1>
+                        <h1>project-1</h1>
+                        <h1>project-2</h1>
+                        <h1>project-3</h1>
+                    </div>
                 </div>
             </div>
         </div>
